b64e0262de76c96aaa174b673eccfc878b846bf2
Jake Faulkner <jakefaulkn@gmail.com>
2024-07-17 16:15:42 +1200

    add generate model coordinates script

---
+++ workflow/scripts/generate_model_coordinates.py
@@ -0,0 +1,118 @@
+"""Generate model station coordinates for EMOD3D."""
+
+from pathlib import Path
+from typing import Annotated
+
+import numpy as np
+import pandas as pd
+import typer
+from qcore import geo
+
+from workflow import realisations
+from workflow.realisations import DomainParameters
+
+
+def generate_fd_files(
+    realisations_ffp: Annotated[
+        Path, typer.Argument(help="Path to realisation json file.", readable=True)
+    ],
+    output_path: Annotated[
+        Path,
+        typer.Argument(
+            help="Output path for station files", file_okay=False, writable=True
+        ),
+    ],
+    keep_dup_station: bool = Annotated[
+        True,
+        typer.Option(help="Keep stations whose gridpoint coordinates are identical"),
+    ],
+    stat_file: Annotated[
+        Path,
+        typer.Option(
+            help="The location of the station files.", readable=True, exists=True
+        ),
+    ] = "/input/stations.ll",
+) -> None:
+    """Generate station gridpoint coordinates for a station list."""
+    output_path.mkdir(exist_ok=True)
+    domain_parameters: DomainParameters = realisations.read_config_from_realisation(
+        DomainParameters,
+    )
+    model_origin = domain_parameters.domain.origin
+    hh = domain_parameters.resolution
+    nx = np.ceil(domain_parameters.domain.extent_x / hh)
+    ny = np.ceil(domain_parameters.domain.extent_x / hh)
+
+    # where to save gridpoint and longlat station files
+    gp_out = output_path / "stations.statcords"
+    ll_out = output_path / "stations.ll"
+
+    # retrieve in station names, latitudes and longitudes
+    stations = pd.read_csv(
+        stat_file, delimiter=" ", comment="#", columns=["name", "lat", "lon"]
+    )
+
+    # convert ll to grid points
+    xy = geo.ll2gp_multi(
+        stations[["lat", "lon"]].to_numpy(),
+        model_origin[1],
+        model_origin[0],
+        domain_parameters.domain.bearing,
+        nx,
+        ny,
+        hh,
+        keep_outside=True,
+    )
+
+    # store gridpoints and names if unique position
+    sxy = []
+    suname = []
+    for i in range(len(xy)):
+        station_name = stations.iloc[i]["name"]
+        if xy[i] is None or xy[i][0] == nx - 1 or xy[i][1] == ny - 1:
+            print(f"Station outside domain: {station_name}")
+        elif xy[i] not in sxy:
+            sxy.append(xy[i])
+            suname.append(station_name)
+        elif keep_dup_station:
+            # still adds in the station but raise a warning
+            sxy.append(xy[i])
+            suname.append(station_name)
+            print(
+                f"Duplicate Station added: {station_name} at {xy[i]}",
+            )
+        else:
+            print(f"Duplicate Station Ignored: {station_name}")
+
+    # create grid point file
+    with open(gp_out, "w", encoding="utf-8") as gpf:
+        # file starts with number of entries
+        gpf.write(f"{sxy}\n")
+        # x, y, z, name
+        for xy, station_name in zip(sxy, suname):
+            gpf.write(f"{xy[0]:5d} {xy:5d} {1:5d} {station_name}")
+
+    # convert unique grid points back to ll
+    # warning: modifies sxy
+    ll = geo.gp2ll_multi(
+        sxy,
+        model_origin[0],
+        model_origin[1],
+        domain_parameters.domain.bearing,
+        nx,
+        ny,
+        hh,
+    )
+
+    # create ll file
+    with open(ll_out, "w", encoding="utf-8") as llf:
+        for pos, station_name in zip(ll, suname):
+            llf.write(f"{pos[0]:11.5f} {pos[1]:11.5f} {station_name}")
+
+
+def main():
+    typer.run(generate_fd_files)
+
+
+if __name__ == "__name__":
+    main()
