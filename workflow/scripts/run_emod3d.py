429b36a3f6c2a1d2d4b52d88d14a57d91873d364
Jake Faulkner <jakefaulkn@gmail.com>
2024-07-19 13:33:11 +1200

    actually run emod3d + docstrings

--- workflow/scripts/run_emod3d.py
+++ workflow/scripts/run_emod3d.py
@@ -1,3 +1,10 @@
+"""
+Run EMOD3D with parameters sourced from a realisation file.
+
+The output of EMOD3D is written to the output directory and then all non-log files are copied to the share directory.
+"""
+
+import subprocess
 from pathlib import Path
 from typing import Annotated
 
@@ -15,6 +22,18 @@ from workflow.realisations import (
 def emod3d_domain_parameters(
     domain_parameters: DomainParameters,
 ) -> dict[str, int | float]:
+    """Create a dictionary of the EMOD3D domain parameters.
+
+    Parameters
+    ----------
+    domain_parameters : DomainParameters
+        The realisation domain parameters.
+
+    Returns
+    -------
+    dict[str, int | float]
+        A dictionary containing the EMOD3D domain parameters.
+    """
     nx = int(np.ceil(domain_parameters.domain.extent_x / domain_parameters.resolution))
     ny = int(np.ceil(domain_parameters.domain.extent_y / domain_parameters.resolution))
     nz = int(np.ceil(domain_parameters.depth / domain_parameters.resolution))
@@ -32,36 +51,97 @@ def emod3d_domain_parameters(
 def emod3d_duration_parameters(
     domain_parameters: DomainParameters, min_vs: float, dtts: float
 ) -> dict[str, int | float]:
+    """Create a dictionary of the EMOD3D duration parameters.
+
+    Parameters
+    ----------
+    domain_parameters : DomainParameters
+        The domain parameters.
+    min_vs : float
+        The minimum velocity.
+    dtts : float
+        The number of dt-increments per timestep.
+
+    Returns
+    -------
+    dict[str, int | float]
+        A dictionary containing the EMOD3D duration parameters.
+    """
     flo = min_vs / (5 * domain_parameters.resolution)
     extended_simulation_duration = domain_parameters.duration + 3 / flo
-    nt = np.round(extended_simulation_duration / domain_parameters.dt)
+    nt = int(np.round(extended_simulation_duration / domain_parameters.dt))
     return {
         "nt": nt,
-        "sim_duration": extended_simulation_duration,
         "dump_itinc": nt,
         "flo": flo,
         "dt": domain_parameters.dt,
-        "ts_total": extended_simulation_duration / (domain_parameters.dt * dtts),
+        "ts_total": int(extended_simulation_duration / (domain_parameters.dt * dtts)),
     }
 
 
 def emod3d_input_directories(
-    srf_file_ffp: Path, velocity_model_ffp: Path, stations_ffp: Path
+    srf_file_ffp: Path, velocity_model_ffp: Path, stations_ffp: Path, grid_ffp: Path
 ) -> dict[str, Path]:
+    """Create a dictionary of the input directories and files for EMOD3D.
+
+    Parameters
+    ----------
+    srf_file_ffp : Path
+        The path to the SRF file.
+    velocity_model_ffp : Path
+        The path to the velocity model directory.
+    stations_ffp : Path
+        The path containing the station files.
+    grid_ffp : Path
+        The path to the grid and model parameter files.
+
+    Raises
+    ------
+    ValueError
+        If any of the specified files or directories do not exist.
+
+    Returns
+    -------
+    dict[str, Path]
+        A dictionary of all the configured input directories.
+    """
     # GRIDFILE & model_params: generated after VM gen,
-    return {
+    input_paths = {
         "faultfile": srf_file_ffp,
         "seiscords": stations_ffp / "stations.statcords",
         "stat_file": stations_ffp / "stations.ll",
+        "grid_file": grid_ffp / "grid_file",
+        "model_params": grid_ffp / "model_params",
         "vel_mod_params_dir": velocity_model_ffp,
         "vmoddir": velocity_model_ffp,
     }
+    for key, path in input_paths.items():
+        if not path.exists():
+            raise ValueError(
+                f"The {key} path does not exist. The path given was {path}"
+            )
+    return input_paths
 
 
 def emod3d_output_directories(scratch_ffp: Path) -> dict[str, Path]:
+    """Create a dictionary of the output directories for EMOD3D.
+
+    This function also creates all the directories if they do not already exist.
+
+    Parameters
+    ----------
+    scratch_ffp : Path
+        The root directory of all output files for the run.
+
+    Returns
+    -------
+    dict[str, Path]
+        A dictionary of all the configured output paths.
+    """
     directories = {
         "main_dump_dir": scratch_ffp / "OutBin",
         "user_scratch": scratch_ffp,
+        "sim_dir": scratch_ffp,
         "seisdir": scratch_ffp / "SeismoBin",
         "restartdir": scratch_ffp / "Restart",
         "logdir": scratch_ffp / "Log",
@@ -76,32 +156,48 @@ def emod3d_output_directories(scratch_ffp: Path) -> dict[str, Path]:
 
 def emod3d_metadata(
     metadata: RealisationMetadata, emod3d_program: Path, emod3d_version: str
-) -> dict[str, Path]:
+) -> dict[str, str | Path]:
+    """Return a dictionary of the EMOD3D metadata parameters.
+
+    Parameters
+    ----------
+    metadata : RealisationMetadata
+        The realisation metadata.
+    emod3d_program : Path
+        The path to the EMOD3D program.
+    emod3d_version : str
+        The version of EMOD3D to use.
+
+    Returns
+    -------
+    dict[str, str | Path]
+        A dictionary containing the metadata parameters for EMOD3D.
+    """
     return {
         "wcc_prog_dir": emod3d_program,
         "version": f"{emod3d_version}-mpi",
         "name": metadata.name,
         "restartname": metadata.name,
-        "n_proc": 512,
     }
 
 
 def format_as_emod3d_value(value: int | float | str | Path) -> str:
+    """Format a value in a format valid for an e3d.par file.
+
+    Parameters
+    ----------
+    value : int | float | str | Path
+        The value to format.
+
+    Returns
+    -------
+    str
+        The value formatted as a string.
+    """
     if isinstance(value, (Path, str)):
         return f'"{value}"'
     else:
         return str(value)
-
-
-def write_emod3d_parameters(
-    e3d_par_ffp: Path, emod3d_parameters: dict[str, int | float | str | Path]
-) -> None:
-    e3d_par_ffp.write_text(
-        "\n".join(
-            f"{key}={format_as_emod3d_value(value)}"
-            for key, value in emod3d_parameters.items()
-        )
-    )
 
 
 def run_emod3d(
@@ -127,13 +223,22 @@ def run_emod3d(
             help="Path to station files", exists=True, readable=True, file_okay=False
         ),
     ],
+    grid_ffp: Annotated[
+        Path,
+        typer.Argument(
+            help="Path to grid coordinate directory",
+            exists=True,
+            readable=True,
+            file_okay=False,
+        ),
+    ],
     output_ffp: Annotated[
         Path,
         typer.Argument(help="Output path", exists=True, writable=True, file_okay=False),
     ],
     scratch_ffp: Annotated[
         Path,
-        typer.Argument(
+        typer.Option(
             help="Scratch filepath for intermediate output.",
             exists=True,
             writable=True,
@@ -156,6 +261,7 @@ def run_emod3d(
         str, typer.Option(help="The version of the EMOD3D binary to use.")
     ] = "3.0.8",
 ):
+    """Run EMOD3D using parameters sourced from a realisation file."""
     domain_parameters: DomainParameters = realisations.read_config_from_realisation(
         DomainParameters, realisation_ffp
     )
@@ -180,7 +286,15 @@ def run_emod3d(
         | emod3d_output_directories(scratch_ffp)
         | emod3d_metadata(metadata, emod3d_path, emod3d_version)
     )
-    write_emod3d_parameters(scratch_ffp / "e3d.par", emod3d_parameters)
+    e3d_par_ffp = scratch_ffp / "e3d.par"
+
+    e3d_par_ffp.write_text(
+        "\n".join(
+            f"{key}={format_as_emod3d_value(value)}"
+            for key, value in emod3d_parameters.items()
+        )
+    )
+    subprocess.check_call([str(emod3d_path), "-args", f"par={e3d_par_ffp}"])
 
 
 def main():
