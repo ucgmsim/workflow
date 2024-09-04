9f152c145507bd20296488ed3f1942953b72160d
Jake Faulkner <jakefaulkn@gmail.com>
2024-09-05 11:30:52 +1200

    better checking of the velocity model parameters

--- tests/test_velocity_model_parameters.py
+++ tests/test_velocity_model_parameters.py
@@ -1,29 +1,28 @@
-import pytest
 import filecmp
+import json
 import shutil
-
 from pathlib import Path
 
-from qcore.data import download_data
+import pytest
 
+from qcore.data import download_data
 from workflow.scripts import generate_velocity_model_parameters
 
 REALISATION_FILES = Path("tests/realisations")
 CORRECT_REALISATION_FILES = Path("tests/realisations/with_velocity_model_parameters")
 
-@pytest.mark.parametrize('rupture_id', [0, 1, 599, 1190])
+
+@pytest.mark.parametrize("rupture_id", [0, 1, 599, 1190])
 def test_velocity_model_parameter_generation(tmp_path: Path, rupture_id: int):
     download_data.download_data()
-    realisation_data = REALISATION_FILES / f'rupture_{rupture_id}.json'
-    test_realisation_path = tmp_path / 'realisation.json'
+    realisation_data = REALISATION_FILES / f"rupture_{rupture_id}.json"
+    test_realisation_path = tmp_path / "realisation.json"
     shutil.copy(realisation_data, test_realisation_path)
 
     generate_velocity_model_parameters.generate_velocity_model_parameters(
         test_realisation_path
     )
-    filecmp.cmp(
-        test_realisation_path,
-        CORRECT_REALISATION_FILES / f'rupture_{rupture_id}.json',
-        shallow=False
-
-    )
+    with open(test_realisation_path, "r") as test_realisation, open(
+        CORRECT_REALISATION_FILES / f"rupture_{rupture_id}.json", "r"
+    ) as correct_realisation:
+        assert json.load(test_realisation) == json.load(correct_realisation)
