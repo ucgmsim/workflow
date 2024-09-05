b9e41692f8c79b91e6022f1151ee87b896efeaf4
Jake Faulkner <jakefaulkn@gmail.com>
2024-09-05 13:33:56 +1200

    Add scripts documentation

--- workflow/scripts/__init__.py
+++ workflow/scripts/__init__.py
@@ -1,1 +1,7 @@
+"""Workflow Scripts.
 
+This sub-package contains all the scripts used in the cybershake
+workflow. They are designed to be re-used in your own workflows. See
+an individual module for more details, or refer to the global
+`workflow` documentation.
+"""
