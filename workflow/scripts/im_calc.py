816e8f1117a55bf4f8f4aa03a624a41c101a266d
Jake Faulkner <jakefaulkn@gmail.com>
2024-10-03 13:08:55 +1300

    first attempt at highly-accelerated im calculation

--- workflow/scripts/im_calc.py
+++ workflow/scripts/im_calc.py
@@ -28,20 +28,104 @@ For More Help
 See the output of `im-calc --help`.
 """
 
-import multiprocessing
+from enum import StrEnum
 from pathlib import Path
-from typing import Annotated
+from typing import Annotated, Callable
 
+import h5py
+import numexpr as ne
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+import tqdm
 import typer
+from pyfftw.interfaces import numpy_fft as fft
 
-from IM_calculation.IM import im_calculation
-from qcore import qclogging
 from workflow.realisations import (
+    BroadbandParameters,
     IntensityMeasureCalcuationParameters,
     RealisationMetadata,
 )
 
 app = typer.Typer()
+
+
+def response_spectra(
+    waveforms: npt.NDArray[np.float32],
+    dt: float,
+    periods: npt.NDArray[np.float32],
+    xi: float = 0.05,
+    max_freq_ratio: int = 5,
+):
+    fourier = fft.rfft(waveforms, axis=0, threads=8)
+    freq = np.linspace(0, 1 / (2 * dt), num=fourier.shape[1])
+    psa = np.zeros(shape=(len(periods), waveforms.shape[1]))
+    i = 0
+    for period in tqdm.tqdm(periods):
+        ang_freq = 2 * np.pi * freq
+        oscillator_freq = 2 * np.pi * period
+        h = -(oscillator_freq**2) / (
+            np.square(ang_freq)
+            - np.square(oscillator_freq)
+            - 2j * xi * oscillator_freq * ang_freq
+        )
+        n = fourier.shape[1]
+        m = max(n, int(max_freq_ratio * oscillator_freq / freq[1]))
+        scale = m / n
+        response = fft.irfft(fourier * h, 2 * (m - 1), axis=0, threads=8)
+        print("computing psa")
+        psa[i] = ne.evaluate("max(abs(scale * response), 0)")
+        i += 1
+    return psa
+
+
+class ComponentWiseOperation(StrEnum):
+    NONE = "cos(theta) * comp_0 + sin(theta) * comp_90"
+    ABS = "abs(cos(theta) * comp_0 + sin(theta) * comp_90)"
+    SQUARE = "(cos(theta) * comp_0 + sin(theta) * comp_90)**2"
+
+
+def compute_in_rotations(
+    waveforms: npt.NDArray[np.float32],
+    function: Callable,
+    component_wise_operation: ComponentWiseOperation | str = ComponentWiseOperation.ABS,
+) -> pd.DataFrame:
+    (stations, nt, _) = waveforms.shape
+    values = np.zeros(shape=(stations, 180), dtype=waveforms.dtype)
+    comp_0 = waveforms[:, :, 0]
+    comp_90 = waveforms[:, :, 1]
+    for i in range(180):
+        theta = np.deg2rad(i)
+        values[:, i] = function(ne.evaluate(component_wise_operation))
+
+    comp_0 = values[:, 0]
+    comp_90 = values[:, 90]
+    rotated_max = np.max(values, axis=1)
+    rotated_median = np.median(values, axis=1)
+    return pd.DataFrame(
+        {
+            "comp_0": comp_0,
+            "comp_90": comp_90,
+            "max": rotated_max,
+            "median": rotated_median,
+        }
+    )
+
+
+def compute_significant_duration(
+    waveforms: npt.NDArray[np.float32],
+    dt: float,
+    percent_low: float,
+    percent_high: float,
+):
+    arias_intensity = np.cumsum(waveforms, axis=1)
+    arias_intensity /= arias_intensity[:, -1][:, np.newaxis]
+    sum_mask = ne.evaluate(
+        "(arias_intensity >= percent_low) & (arias_intensity <= percent_high)"
+    )
+
+    threshold_values = np.count_nonzero(sum_mask, axis=1) * dt
+    return threshold_values.ravel()
 
 
 @app.command(help="Calculate instensity measures for simulation data.")
@@ -65,8 +149,8 @@ def calculate_instensity_measures(
         ),
     ],
     simulated_stations: Annotated[
-        bool, typer.Option(help='If passed, calculate for simulated stations.')
-    ] = True
+        bool, typer.Option(help="If passed, calculate for simulated stations.")
+    ] = True,
 ):
     """Calculate intensity measures for simulation data.
 
@@ -81,38 +165,79 @@ def calculate_instensity_measures(
     """
     output_path.mkdir(exist_ok=True)
     metadata = RealisationMetadata.read_from_realisation(realisation_ffp)
+    broadband_parameters = BroadbandParameters.read_from_realisation(realisation_ffp)
     intensity_measure_parameters = (
         IntensityMeasureCalcuationParameters.read_from_realisation_or_defaults(
             realisation_ffp, metadata.defaults_version
         )
     )
 
-    logger = qclogging.get_logger("IM_calc")
-    logger.info("IM_Calc started")
+    with h5py.File(broadband_simulation_ffp, mode="r") as broadband_file:
+        waveforms = np.array(broadband_file["waveforms"])
 
-    im_options = {
-        "pSA": intensity_measure_parameters.valid_periods,
-        "SDI": intensity_measure_parameters.valid_periods,
-        "FAS": intensity_measure_parameters.fas_frequencies,
-    }
-
-    im_calculation.compute_measures_multiprocess(
-        broadband_simulation_ffp,
-        "binary",  # hard-code for binary file format for broadband waveforms
-        wave_type=None,
-        station_names=None,  # Passing None -> runs for all stations in the file
-        ims=intensity_measure_parameters.ims,
-        comp=intensity_measure_parameters.components,
-        im_options=im_options,
-        output=output_path,
-        identifier="realisation",
-        rupture="realisation",
-        run_type="simulated",
-        version="XXpY",  # default value. Not needed?
-        process=multiprocessing.cpu_count(),
-        simple_output=True,
-        units=intensity_measure_parameters.units,
-        advanced_im_config=None,
-        real_only=(not simulated_stations),
-        logger=logger,
-    )
+    stations = pd.read_hdf(broadband_simulation_ffp, key="stations")
+    pga = compute_in_rotations(waveforms, lambda v: v.max(axis=1))  # ~30s
+    pgv = compute_in_rotations(
+        np.cumsum(waveforms, axis=1) * 981 * broadband_parameters.dt,
+        lambda v: v.max(axis=1),
+    )  # ~ 30s
+    cav = compute_in_rotations(
+        waveforms, lambda v: np.trapz(v, dx=broadband_parameters.dt, axis=1)
+    )  # ~ 30s
+    ai = compute_in_rotations(
+        waveforms,
+        lambda v: np.trapz(v, dx=broadband_parameters.dt, axis=1),
+        component_wise_operation=ComponentWiseOperation.SQUARE,
+    )  # ~ 30s
+    ds575 = compute_in_rotations(
+        waveforms,
+        lambda v: compute_significant_duration(v, broadband_parameters.dt, 5, 75),
+        component_wise_operation=ComponentWiseOperation.SQUARE,
+    )  # ~ 45s
+    # psa = response_spectra(
+    #     waveforms[:, :, 0],
+    #     broadband_parameters.dt,
+    #     np.array(
+    #         [
+    #             0.01,
+    #             0.02,
+    #             0.03,
+    #             0.04,
+    #             0.05,
+    #             0.075,
+    #             0.1,
+    #             0.12,
+    #             0.15,
+    #             0.17,
+    #             0.2,
+    #             0.25,
+    #             0.3,
+    #             0.4,
+    #             0.5,
+    #             0.6,
+    #             0.7,
+    #             0.75,
+    #             0.8,
+    #             0.9,
+    #             1.0,
+    #             1.25,
+    #             1.5,
+    #             2.0,
+    #             2.5,
+    #             3.0,
+    #             4.0,
+    #             5.0,
+    #             6.0,
+    #             7.5,
+    #             10.0,
+    #         ]
+    #     ),
+    # )
+    # print(psa)
+    # print(sys.getsizeof(pgv))
+    print(stations)
+    # print(ds575)
+    print(pga)
+    # print(pgv)
+    # print(cav)
+    # print(ai)
