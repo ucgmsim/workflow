6f23687f3ac420750638903600c1190b53d8bd71
Jake Faulkner <jakefaulkn@gmail.com>
2024-10-10 16:20:41 +1300

    fixes for auto simulation script

--- gcmt/gcmt_auto_simulate.bash
+++ gcmt/gcmt_auto_simulate.bash
@@ -5,13 +5,17 @@ OLD_GCMT="/mnt/hypo_scratch/jfa92/automated_gcmt/gcmt_solutions.csv"
 
 wget -O $NEW_GCMT "https://raw.githubusercontent.com/GeoNet/data/main/moment-tensor/GeoNet_CMT_solutions.csv"
 
-MOST_RECENT_OLD_GCMT=$(awk -F',' '{id=max(id, $2)} END{print $2}' $OLD_GCMT)
+if [[ -f $OLD_GCMT ]]; then
+    cp $OLD_GCMT $NEW_GCMT
+fi
+
+MOST_RECENT_OLD_GCMT=$(awk -F',' '$2 > $id {id=$2} END{print $2}' $OLD_GCMT)
 pyenv activate workflow
 while IFS= read -r gcmt_id; do
     echo "Will simulate GCMT event id: $gcmt_id"
     mkdir -p ~/cylc-src/"$gcmt_id"/input
     plan-workflow "$gmct_id" "$HOME/cylc-src/$gcmt_id/flow.cylc" --goal im_calc --goal plot_ts --source gcmt --defaults-version 24.2.2.4
     echo "bash -c cylc vip $gcmt_id" | batch
-done < <(awk -F',' "\$2 > $MOST_RECENT_OLD_GCMT &&\$12 >= 4.5 { print $1 }")
+done < <(awk -F',' "\$2 > $MOST_RECENT_OLD_GCMT &&\$12 >= 4.5 { print $1 }" $NEW_GCMT)
 
 mv $OLD_GCMT $NEW_GCMT
