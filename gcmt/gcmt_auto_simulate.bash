e2d322400e30dc170530a9f0af7817b832829398
Jake Faulkner <jakefaulkn@gmail.com>
2024-10-10 16:12:55 +1300

    Add automatic simulation script

---
+++ gcmt/gcmt_auto_simulate.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+NEW_GCMT="/mnt/hypo_scratch/jfa92/automated_gcmt/gcmt_solutions.csv.new"
+OLD_GCMT="/mnt/hypo_scratch/jfa92/automated_gcmt/gcmt_solutions.csv"
+
+wget -O $NEW_GCMT "https://raw.githubusercontent.com/GeoNet/data/main/moment-tensor/GeoNet_CMT_solutions.csv"
+
+MOST_RECENT_OLD_GCMT=$(awk -F',' '{id=max(id, $2)} END{print $2}' $OLD_GCMT)
+
+while IFS= read -r gcmt_id; do
+    echo "Will simulate GCMT event id: $gcmt_id"
+    mkdir ~/cylc-src/"$gcmt_id"
+    plan-workflow "$gmct_id" --goal im_calc --goal plot_ts --source gcmt --defaults-version 24.2.2.4
+    echo "bash -c cylc vip $gcmt_id" | batch
+done < <(awk -F',' "\$2 > $MOST_RECENT_OLD_GCMT &&\$12 >= 4.5 { print $1 }")
