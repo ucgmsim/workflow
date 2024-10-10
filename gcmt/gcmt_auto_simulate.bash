1e5f044ee66ec6cc670ba0270fa8100d44da8965
Jake Faulkner <jakefaulkn@gmail.com>
2024-10-11 09:05:58 +1300

    set target host and quote  cylc install command

--- gcmt/gcmt_auto_simulate.bash
+++ gcmt/gcmt_auto_simulate.bash
@@ -14,8 +14,8 @@ source $HOME/.pyenv/versions/workflow/bin/activate
 while IFS= read -r gcmt_id; do
     echo "Will simulate GCMT event id: $gcmt_id"
     mkdir -p ~/cylc-src/"gcmt_$gcmt_id"/input
-    plan-workflow "$gmct_id" "$HOME/cylc-src/gcmt_$gcmt_id/flow.cylc" --goal im_calc --goal plot_ts --source gcmt --defaults-version 24.2.2.4
-    echo "source $HOME/.pyenv/versions/workflow/bin/activate &&bash -c cylc vip gcmt_$gcmt_id" | batch
+    plan-workflow "$gmct_id" "$HOME/cylc-src/gcmt_$gcmt_id/flow.cylc" --goal im_calc --goal plot_ts --source gcmt --defaults-version 24.2.2.4 --target-host hypocentre
+    echo "source $HOME/.pyenv/versions/workflow/bin/activate &&bash -c 'cylc vip gcmt_$gcmt_id'" | batch
 done < <(awk -F',' "\$2 > $MOST_RECENT_OLD_GCMT &&\$12 >= 4.5 && \$1 !=\"PublicID\" { print \$1 }" $NEW_GCMT)
 
 mv $NEW_GCMT $OLD_GCMT
