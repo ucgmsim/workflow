559507348f14b0a5e44eafb9883e2ebba590089b
Jake Faulkner <jakefaulkn@gmail.com>
2024-10-31 10:33:47 +1300

    revert bb sim changes

--- workflow/scripts/bb_sim.py
+++ workflow/scripts/bb_sim.py
@@ -315,7 +315,7 @@ def combine_hf_and_lf(
     )
     stations["waveform_index"] = np.arange(len(stations))
     # ensure that LF and HF agree on station list, sometimes LF can drop a station or two
-    stations = stations.loc[lf.stations.index]
+    stations = stations.loc[lf.stations["name"]]
 
     station_vs30 = pd.read_csv(
         station_vs30_ffp,
