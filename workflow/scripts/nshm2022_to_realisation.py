ae57a8e87599a54f038802df8ec36d38414c3a4c
Jake Faulkner <jakefaulkn@gmail.com>
2025-03-07 13:45:20 +1300

    fix(nshm2022-to-realisation): pass the dataframe not the VelocityModel1D class

--- workflow/scripts/nshm2022_to_realisation.py
+++ workflow/scripts/nshm2022_to_realisation.py
@@ -334,7 +334,7 @@ def generate_realisation(
         fault_name: fault_info.rake for fault_name, fault_info in faults_info.items()
     }
     avg_rake = np.mean(list(rakes.values()))
-    magnitudes = default_magnitude_estimation(faults, velocity_model, avg_rake)
+    magnitudes = default_magnitude_estimation(faults, velocity_model.model, avg_rake)
     if lat_hypo is not None and lon_hypo is not None:
         initial_fault, hypocentre = find_fault_and_hypocentre(
             faults, lat_hypo, lon_hypo
