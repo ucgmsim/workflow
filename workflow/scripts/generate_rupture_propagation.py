a6196c4248ebbb4ffd64ee3651625e741ab8ab2c
Jake Faulkner <jakefaulkn@gmail.com>
2025-05-15 14:46:18 +1200

    refactor(generate-rupture-propagation): update rupture propagation

--- workflow/scripts/generate_rupture_propagation.py
+++ workflow/scripts/generate_rupture_propagation.py
@@ -40,101 +40,18 @@ import numpy as np
 import typer
 
 from qcore import cli
-from qcore.uncertainties import distributions, mag_scaling
+from qcore.uncertainties import distributions
 from source_modelling import rupture_propagation
-from source_modelling.sources import Fault
+
 from workflow import realisations
 
 app = typer.Typer()
-
-
-def a_to_mw_leonard(area: float, rake: float) -> float:
-    """
-    Convert fault area and rake to moment magnitude using the Leonard scaling relation.
-
-    Parameters
-    ----------
-    area : float
-        The area of the fault in square kilometres.
-    rake : float
-        The rake angle of the fault in degrees.
-
-    Returns
-    -------
-    float
-        The estimated moment magnitude of the fault.
-
-    References
-    ----------
-    Leonard, M. (2010). Earthquake fault scaling: Self-consistent
-    relating of rupture length, width, average displacement, and
-    moment release. Bulletin of the Seismological Society of America,
-    100(5A), 1971-1988.
-    """
-    return mag_scaling.a_to_mw_leonard(area, 4, 3.99, rake)
-
-
-def default_magnitude_estimation(
-    faults: dict[str, Fault], rakes: dict[str, float]
-) -> dict[str, float]:
-    """Estimate the magnitudes for a set of faults based on their areas and average rake.
-
-    Parameters
-    ----------
-    faults : dict
-        A dictionary where the keys are fault names and the values are `Fault` objects containing information about each fault.
-    rakes : dict
-        A dictionary where the keys are fault names and the values are rake angles (in degrees) for each fault.
-
-    Returns
-    -------
-    dict
-        A dictionary where the keys are fault names and the values are the estimated magnitudes for each fault.
-    """
-    total_area = sum(fault.area() for fault in faults.values())
-    avg_rake = np.mean(list(rakes.values()))
-    estimated_mw = a_to_mw_leonard(total_area, avg_rake)
-    estimated_moment = mag_scaling.mag2mom(estimated_mw)
-    return {
-        fault_name: mag_scaling.mom2mag((fault.area() / total_area) * estimated_moment)
-        for fault_name, fault in faults.items()
-    }
-
-
-def rake_parser(rake_value: str) -> dict[str, float]:
-    """Parse a rake key=value pair list.
-
-    Parameters
-    ----------
-    rake_value : str
-        The input key=value list.
-
-    Returns
-    -------
-    dict[str, float]
-        The output rake dictionary.
-
-    Examples
-    --------
-    >>> rake_parser('Acton=110')
-    {"Acton": 110}
-    """
-    rake_key_values = rake_value.split(",")
-    rakes = {}
-    for kv_pair in rake_key_values:
-        fault_name, fault_rake = kv_pair.split("=")
-        rakes[fault_name.replace("_", " ")] = float(fault_rake)
-    return rakes
 
 
 @cli.from_docstring(app)
 def generate_rupture_propagation(
     realisation_ffp: Annotated[Path, typer.Argument()],
     initial_fault: Annotated[str, typer.Argument()],
-    rakes: Annotated[
-        dict[str, float],
-        typer.Argument(parser=rake_parser),
-    ],
     shypo: Annotated[Optional[float], typer.Option(min=0, max=1)] = None,
     dhypo: Annotated[Optional[float], typer.Option(min=0, max=1)] = None,
 ):
@@ -174,12 +91,10 @@ def generate_rupture_propagation(
     )
 
     rupture_propagation_config = realisations.RupturePropagationConfig(
-        magnitudes=default_magnitude_estimation(faults, rakes),
         rupture_causality_tree=rupture_causality_tree,
         jump_points=rupture_propagation.jump_points_from_rupture_tree(
             faults, rupture_causality_tree
         ),
-        rakes=rakes,
         hypocentre=hypocentre,
     )
 
