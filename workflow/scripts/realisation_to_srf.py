bf85b5d0caa7f308381292f0bbf2484b15229f64
AndrewRidden-Harper <52001209+AndrewRidden-Harper@users.noreply.github.com>
2025-07-28 10:09:28 +1200

    update single threaded logic
    Co-authored-by: Jake Faulkner <jakefaulkn@gmail.com>

--- workflow/scripts/realisation_to_srf.py
+++ workflow/scripts/realisation_to_srf.py
@@ -502,11 +502,7 @@ def generate_fault_srfs_multi(
 
     # In practice, a Point source will be the only item in `faults`, so if there
     # is a Point source, restrict to `single_threaded` mode for simplicity.
-    for name in faults:
-        if isinstance(faults[name], Point):
-            single_threaded = True
-            break
-
+    single_threaded = single_threaded or len(faults) == 1
     if single_threaded:
         for name in faults:
             if isinstance(faults[name], Point):
