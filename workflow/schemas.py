d9861cb63e43b3e998a78ce7d00374ba7a2cd9ce
AndrewRidden-Harper <52001209+AndrewRidden-Harper@users.noreply.github.com>
2025-07-29 16:35:14 +1200

    add PointSourceParams to schemas.py
    Co-authored-by: Jake Faulkner <jakefaulkn@gmail.com>

--- workflow/schemas.py
+++ workflow/schemas.py
@@ -52,7 +52,24 @@ class Stype(StrEnum):
     cos = "cos"
     seki = "seki"
 
+@dataclasses.dataclass
+class PointSourceParams:
+    """Parameters for point source approximation."""
 
+    stype: schemas.Stype
+    """Slip type for generic_slip2srf"""
+
+    risetime: float
+    """Rise time for generic_slip2srf"""
+
+    risetimefac: float
+    """Rise time factor for generic_slip2srf"""
+
+    risetimedep: float
+    """Rise time depth dependency for generic_slip2srf"""
+    
+    inittime: float
+    """Initial time for generic_slip2srf"""
 def _is_positive(x: float) -> bool:  # noqa: D103 # numpydoc ignore=GL08
     return x > 0
 
