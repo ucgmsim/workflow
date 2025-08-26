8a85c1cc58db1ff4cb57432d195ac2d27925909b
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2025-08-27 10:28:23 +1200

    fix(generate-velocity-model): correctly set OMP_NUM_THREADS

--- workflow/scripts/generate_velocity_model.py
+++ workflow/scripts/generate_velocity_model.py
@@ -41,7 +41,7 @@ from typing import Annotated, Optional
 import typer
 
 from qcore import cli
-from workflow import log_utils, realisations
+from workflow import log_utils, realisations, utils
 from workflow.realisations import (
     DomainParameters,
     RealisationMetadata,
@@ -110,8 +110,9 @@ def run_nzvm(
         None for inferred thread count.
     """
     environment = os.environ.copy()
-    if num_threads is None and "OMP_NUM_THREADS" in environment:
-        environment.pop("OMP_NUM_THREADS")
+
+    if num_threads is None:
+        environment["OMP_NUM_THREADS"] = str(utils.get_available_cores())
     elif num_threads:
         environment["OMP_NUM_THREADS"] = str(num_threads)
 
