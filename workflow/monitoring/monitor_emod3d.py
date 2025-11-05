b585bb6497e32e58c908ed3a9b90aa8478ca9d4e
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2025-11-06 12:09:56 +1300

    docs(monitor_emod3d): document monitoring process

--- workflow/monitoring/monitor_emod3d.py
+++ workflow/monitoring/monitor_emod3d.py
@@ -1,3 +1,5 @@
+"""Monitor EMOD3D job progress from the command line."""
+
 import asyncio
 import re
 import time
@@ -5,9 +7,12 @@ from dataclasses import dataclass
 from datetime import timedelta
 from enum import Enum, auto
 from pathlib import Path
+from typing import Annotated
 
 import typer
 from rich.progress import BarColumn, Progress, TextColumn, TimeRemainingColumn
+
+from qcore import cli
 
 app = typer.Typer()
 
@@ -42,7 +47,22 @@ STEP_REGEX = re.compile(
 def find_fresh_files(
     root: Path, now: float, file_glob: str, stale_seconds: int
 ) -> list[Path]:
-    """Find all files matching a given file glob in a directory modified after a given time."""
+    """Recursively find all files matching a given file glob in a directory modified after a given time.
+
+    Parameters
+    ----------
+    root : Path
+        The path to begin searching for.
+    file_glob : str
+        The file glob for files to consider.
+    stale_seconds : int
+        The number of seconds to consider a file as recently modified.
+
+    Returns
+    -------
+    list[Path]
+        The paths matching `file_glob` modified within the last `stale_seconds` seconds.
+    """
     return [
         f.resolve()
         for f in root.rglob(file_glob)
@@ -51,26 +71,57 @@ def find_fresh_files(
 
 
 class EventType(Enum):
+    """Event type enum."""
+
     CREATED = auto()
+    """File was created."""
     MODIFIED = auto()
+    """File was modified."""
     STALE = auto()
+    """File has been deleted or was not recently modified."""
 
 
 @dataclass
 class ProgressUpdate:
+    """Dataclass describing file updates."""
+
     event_type: EventType
+    """The type of event."""
     path: Path
+    """The path of the file updated."""
     nt: int | None = None
+    """The discovered total number of timesteps."""
     current: int | None = None
+    """The current timestep."""
     cumulative_time: int | None = None
+    """The cumulative time for the simulation since the first timestep."""
+    # Exponential weighted moving average is used here instead of simple
+    # average to allow the process to adapt to transient changes in the cluster
+    # speed, and to converge quickly to a good estimate of completion time
+    # (the first 100 timesteps are typically the slowest).
     tps: float | None = None
+    """Timesteps per second (exponential weighted average)."""
 
 
 def log_progress_update(event_path: Path) -> ProgressUpdate:
+    """Parse an EMOD3D log file and return a progress update.
+
+    Parameters
+    ----------
+    event_path : Path
+        The path of the log file to read.
+
+    Returns
+    -------
+    ProgressUpdate
+        The simulation's current progress.
+    """
     nt = None
     current = None
     cumulative_time = None
     tps = None
+    # An alpha = 0.3 means that 30% of the average tps value is represented
+    # by the time to compute the last 100 timesteps.
     alpha = 0.3
     with open(event_path, "r") as fp:
         for line in fp:
@@ -104,10 +155,40 @@ async def monitor_files(
     file_glob: str,
     stale_seconds: int,
 ) -> None:
-    tracked: dict[Path, int] = {}
+    """Monitor LF log files in a given directory and publish updates to an event queue.
+
+    Parameters
+    ----------
+    event_queue : Queue
+        The event queue to publish updates to.
+    root : Path
+        The path to watch for new LF log files in.
+    file_glob : str
+        The glob pattern used to match LF log files.
+    stale_seconds : int
+        The number of seconds that must pass for a file to be considered stale to stop watching.
+    """
+    # Contains a dictionary of path -> modified time. This is the most reliable
+    # way to track file changes without hashing. Three other methods were
+    # also considered:
+    #
+    # 1. INotify. This is canonical on local filesystems, but doesn't work
+    # on network mounted filesystems (i.e. most filesystems on HPC) because
+    # they don't typically implement inotify.
+    #
+    # 2. Using a global modified time and comparing file modification times to
+    # the global modified time - poll interval. Network mounted filesystems
+    # record the modification time on the filesystem they've mounted. They
+    # can take some time to sync changes, which means that you often miss
+    # updates that occur out-of-order.
+    #
+    # 3. Using a dictionary of path -> file size. This works just fine but
+    # is less robust that checking if the file is modified. It also means
+    # you can't prompt an update using touch.
+    tracked: dict[Path, float] = {}
     for tracked_file in find_fresh_files(root, time.time(), file_glob, stale_seconds):
         await queue.put(ProgressUpdate(EventType.CREATED, tracked_file))
-        tracked[tracked_file] = tracked_file.stat().st_size
+        tracked[tracked_file] = tracked_file.stat().st_mtime
         await queue.put(log_progress_update(tracked_file))
 
     poll_interval = 5
@@ -120,10 +201,10 @@ async def monitor_files(
             await queue.put(ProgressUpdate(EventType.STALE, stale_file))
             del tracked[stale_file]
         for updated_file in tracked_files & fresh_files:
-            size = updated_file.stat().st_size
-            if tracked[updated_file] != size:
+            mtime = updated_file.stat().st_mtime
+            if tracked[updated_file] != mtime:
                 await queue.put(log_progress_update(updated_file))
-                tracked[updated_file] = size
+                tracked[updated_file] = mtime
         for tracked_file in fresh_files - tracked_files:
             await queue.put(ProgressUpdate(EventType.CREATED, tracked_file))
             tracked[tracked_file] = tracked_file.stat().st_size
@@ -135,7 +216,22 @@ async def track_rlog_progress(
     file_glob: str,
     suffix: str,
     stale_seconds: int,
-):
+) -> None:
+    """Track LF progress using Rich progress bars.
+
+    Parameters
+    ----------
+    event_queue : Queue
+        The event queue to publish updates to.
+    root : Path
+        The path to watch for new LF log files in.
+    file_glob : str
+        The glob pattern used to match LF log files.
+    suffix : str
+        The common suffix of rlog files to strip for presenting the name with the progress bars.
+    stale_seconds : int
+        The number of seconds that must pass for a file to be considered stale to stop watching.
+    """
     asyncio.create_task(monitor_files(event_queue, root, file_glob, stale_seconds))
     progress = Progress(
         TextColumn("[bold blue]{task.fields[filename]}", justify="right"),
@@ -179,16 +275,30 @@ async def track_rlog_progress(
                     task_id = progress_tasks.pop(event.path)
                     progress.remove_task(task_id)
                 case None:
+                    # This will occur if any early termination logic is implemented.
                     return
 
 
-@app.command()
+@cli.from_docstring(app)
 def monitor_lf(
-    log_dir: Path = Path("."),
+    log_dir: Annotated[Path, typer.Option(file_okay=False, readable=True)] = Path("."),
     file_glob: str = "*.rlog",
-    stale_seconds: int = 5 * 60,
+    stale_seconds: Annotated[int, typer.Option(min=1)] = 5 * 60,
     suffix: str = "-00000.rlog",
 ) -> None:
+    """Monitor a directory for LF progress and publish the results as progress bars.
+
+    Paramaters
+    ----------
+    log_dir : Path
+        The directory to search for log files.
+    file_glob : str
+        A glob for files to consider for processing.
+    stale_seconds : int
+        The timeout for a file to be considered stale (usually this means EMOD3D terminated). These jobs are removed.
+    suffix : str
+        The suffix to strip from the run log files when displaying them alongside progress.
+    """
     event_queue = asyncio.Queue()
 
     asyncio.run(
