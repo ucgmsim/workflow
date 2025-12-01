fix: extract resolution and dt and align computation

tests/test_realisation.py
@@ -21,7 +21,6 @@ from workflow.realisations import SourceConfig
 
 def test_bounding_box_example(tmp_path: Path) -> None:
     domain_parameters = realisations.DomainParameters(
-        resolution=0.1,  # a 0.1km resolution
         domain=bounding_box.BoundingBox.from_centroid_bearing_extents(
             centroid=np.array([-43.53092, 172.63701]),
             bearing=45.0,
@@ -30,14 +29,12 @@ def test_bounding_box_example(tmp_path: Path) -> None:
         ),
         depth=40.0,
         duration=60.0,
-        dt=0.005,
     )
     realisation_ffp = tmp_path / "realisation.json"
     domain_parameters.write_to_realisation(realisation_ffp)
     with open(realisation_ffp, "r") as realisation_handle:
         assert json.load(realisation_handle) == {
             "domain": {
-                "resolution": 0.1,
                 "domain": [
                     {"latitude": -43.524793866326725, "longitude": 171.76204128885567},
                     {"latitude": -44.16756820707226, "longitude": 172.63312824122775},
@@ -46,7 +43,6 @@ def test_bounding_box_example(tmp_path: Path) -> None:
                 ],
                 "depth": 40.0,
                 "duration": 60.0,
-                "dt": 0.005,
             }
         }
     domain_parameters_read = realisations.DomainParameters.read_from_realisation(
@@ -54,8 +50,6 @@ def test_bounding_box_example(tmp_path: Path) -> None:
     )
     assert domain_parameters_read.depth == domain_parameters.depth
     assert domain_parameters_read.duration == domain_parameters.duration
-    assert domain_parameters_read.dt == domain_parameters.dt
-    assert domain_parameters_read.resolution == domain_parameters.resolution
     assert (
         domain_parameters_read.domain.corners == domain_parameters.domain.corners
     ).all()
@@ -63,7 +57,6 @@ def test_bounding_box_example(tmp_path: Path) -> None:
 
 def test_domain_parameters_properties() -> None:
     domain_parameters = realisations.DomainParameters(
-        resolution=0.1,  # a 0.1km resolution
         domain=bounding_box.BoundingBox.from_centroid_bearing_extents(
             centroid=np.array([-43.53092, 172.63701]),
             bearing=45.0,
@@ -72,17 +65,11 @@ def test_domain_parameters_properties() -> None:
         ),
         depth=40.0,
         duration=60.0,
-        dt=0.005,
     )
-
-    assert domain_parameters.nx == 1000
-    assert domain_parameters.ny == 1000
-    assert domain_parameters.nz == 400
 
 
 def test_srf_config_example(tmp_path: Path) -> None:
     domain_parameters = realisations.DomainParameters(
-        resolution=0.1,  # a 0.1km resolution
         domain=bounding_box.BoundingBox.from_centroid_bearing_extents(
             centroid=np.array([-43.53092, 172.63701]),
             bearing=45.0,
@@ -91,12 +78,9 @@ def test_srf_config_example(tmp_path: Path) -> None:
         ),
         depth=40.0,
         duration=60.0,
-        dt=0.005,
     )
     srf_config = realisations.SRFConfig(
-        genslip_dt=1.0,
         genslip_version="5.4.2",
-        resolution=0.1,
         point_source_params=schemas.PointSourceParams(
             stype=schemas.Stype.cos,
             risetime=0.5,
@@ -112,7 +96,6 @@ def test_srf_config_example(tmp_path: Path) -> None:
     with open(realisation_ffp, "r") as realisation_handle:
         assert json.load(realisation_handle) == {
             "domain": {
-                "resolution": 0.1,
                 "domain": [
                     {"latitude": -43.524793866326725, "longitude": 171.76204128885567},
                     {"latitude": -44.16756820707226, "longitude": 172.63312824122775},
@@ -121,11 +104,8 @@ def test_srf_config_example(tmp_path: Path) -> None:
                 ],
                 "depth": 40.0,
                 "duration": 60.0,
-                "dt": 0.005,
             },
             "srf": {
-                "genslip_dt": 1.0,
-                "resolution": 0.1,
                 "genslip_version": "5.4.2",
                 "point_source_params": {
                     "stype": "cos",
@@ -140,49 +120,12 @@ def test_srf_config_example(tmp_path: Path) -> None:
     assert realisations.SRFConfig.read_from_realisation(realisation_ffp) == srf_config
 
 
-def test_bad_domain_parameters(tmp_path: Path) -> None:
-    bad_json = tmp_path / "bad_domain_parameters.json"
-    bad_json.write_text(
-        json.dumps(
-            {
-                "domain": {
-                    "resolution": 0,  # Set to 0
-                    "domain": [
-                        {
-                            "latitude": -43.524793866326725,
-                            "longitude": 171.76204128885567,
-                        },
-                        {
-                            "latitude": -42.894200350955856,
-                            "longitude": 172.64076673694242,
-                        },
-                        {
-                            "latitude": -43.53034935969409,
-                            "longitude": 173.51210368762364,
-                        },
-                        {
-                            "latitude": -44.16756820707226,
-                            "longitude": 172.63312824122775,
-                        },
-                    ],
-                    "depth": 40.0,
-                    "duration": 60.0,
-                    "dt": 0.005,
-                }
-            }
-        )
-    )
-    with pytest.raises(schema.SchemaError):
-        realisations.DomainParameters.read_from_realisation(bad_json)
-
-
 def test_bad_config_key(tmp_path: Path) -> None:
     bad_json = tmp_path / "bad_domain_parameters.json"
     bad_json.write_text(
         json.dumps(
             {
                 "not the correct domain key": {
-                    "resolution": 0.1,
                     "domain": [
                         {
                             "latitude": -43.524793866326725,
@@ -203,7 +146,6 @@ def test_bad_config_key(tmp_path: Path) -> None:
                     ],
                     "depth": 40.0,
                     "duration": 60.0,
-                    "dt": 0.005,
                 }
             }
         )
@@ -241,9 +183,7 @@ def test_velocity_model(tmp_path: Path) -> None:
         min_vs=1.0,
         version="2.06",
         topo_type="SQUASHED_TAPERED",
-        dt=0.05,
         ds_multiplier=1.2,
-        resolution=0.1,
         vs30=300.0,
         s_wave_velocity=3500.0,
         pgv_interpolants=np.ones(shape=(2, 2), dtype=np.float32),
@@ -256,9 +196,7 @@ def test_velocity_model(tmp_path: Path) -> None:
                 "min_vs": 1.0,
                 "version": "2.06",
                 "topo_type": "SQUASHED_TAPERED",
-                "dt": 0.05,
                 "ds_multiplier": 1.2,
-                "resolution": 0.1,
                 "vs30": 300.0,
                 "s_wave_velocity": 3500.0,
                 "pgv_interpolants": [[1, 1], [1, 1]],
@@ -376,7 +314,7 @@ def test_hf_config(tmp_path: Path) -> None:
     hf_config.write_to_realisation(test_realisation)
     assert realisations.HFConfig.read_from_realisation(test_realisation) == hf_config
     # Test that realisation parameters override defaults.
-    hf_config.dt = 0.1
+    hf_config.czero = 2.0
     hf_config.write_to_realisation(test_realisation)
     assert (
         realisations.HFConfig.read_from_realisation_or_defaults(
@@ -408,14 +346,13 @@ def test_emod3d(tmp_path: Path) -> None:
 def test_broadband_parameters(tmp_path: Path) -> None:
     test_realisation = tmp_path / "realisation.json"
     broadband_parameters = realisations.BroadbandParameters(
-        flo=0.5, dt=0.005, fmidbot=0.5, fmin=0.25, site_amp_version="2014"
+        flo=0.5, fmidbot=0.5, fmin=0.25, site_amp_version="2014"
     )
     broadband_parameters.write_to_realisation(test_realisation)
     with open(test_realisation, "r") as realisation_handle:
         assert json.load(realisation_handle) == {
             "bb": {
                 "flo": 0.5,
-                "dt": 0.005,
                 "fmidbot": 0.5,
                 "fmin": 0.25,
                 "site_amp_version": "2014",

workflow/scripts/create_e3d_par.py
@@ -40,6 +40,7 @@ from workflow.realisations import (
     DomainParameters,
     EMOD3DParameters,
     RealisationMetadata,
+    Resolution,
     VelocityModelParameters,
 )
 
@@ -47,6 +48,7 @@ app = typer.Typer()
 
 
 def emod3d_domain_parameters(
+    resolution: Resolution,
     domain_parameters: DomainParameters,
 ) -> dict[str, int | float]:
     """Create a dictionary of the EMOD3D domain parameters.
@@ -61,11 +63,15 @@ def emod3d_domain_parameters(
     dict[str, int | float]
         A dictionary containing the EMOD3D domain parameters.
     """
+
+    nx = int((domain_parameters.domain.extent_x / resolution.resolution) + 0.5)
+    ny = int((domain_parameters.domain.extent_y / resolution.resolution) + 0.5)
+    nz = int((domain_parameters.depth / resolution.resolution) + 0.5)
     return {
-        "nx": domain_parameters.nx,
-        "ny": domain_parameters.ny,
-        "nz": domain_parameters.nz,
-        "h": domain_parameters.resolution,
+        "nx": nx,
+        "ny": ny,
+        "nz": nz,
+        "h": resolution.resolution,
         "modellat": float(domain_parameters.domain.origin[0]),
         "modellon": float(domain_parameters.domain.origin[1]),
         "modelrot": float(domain_parameters.domain.great_circle_bearing),
@@ -73,7 +79,10 @@ def emod3d_domain_parameters(
 
 
 def emod3d_duration_parameters(
-    domain_parameters: DomainParameters, min_vs: float, dtts: float
+    resolution: Resolution,
+    domain_parameters: DomainParameters,
+    min_vs: float,
+    dtts: float,
 ) -> dict[str, int | float]:
     """Create a dictionary of the EMOD3D duration parameters.
 
@@ -91,15 +100,15 @@ def emod3d_duration_parameters(
     dict[str, int | float]
         A dictionary containing the EMOD3D duration parameters.
     """
-    flo = min_vs / (5 * domain_parameters.resolution)
+    flo = min_vs / (5 * resolution.resolution)
     extended_simulation_duration = domain_parameters.duration + 3 / flo
-    nt = int(np.round(extended_simulation_duration / domain_parameters.dt))
+    nt = int(np.round(extended_simulation_duration / resolution.dt))
     return {
         "nt": nt,
         "dump_itinc": nt,
         "flo": flo,
-        "dt": domain_parameters.dt,
-        "ts_total": int(extended_simulation_duration / (domain_parameters.dt * dtts)),
+        "dt": resolution.dt,
+        "ts_total": int(extended_simulation_duration / (resolution.dt * dtts)),
         "restart_itinc": round(nt / 3),
     }
 
@@ -250,13 +259,17 @@ def create_e3d_par(
         realisation_ffp
     )
     metadata = RealisationMetadata.read_from_realisation(realisation_ffp)
+    resolution = Resolution.read_from_realisation_or_defaults(
+        realisation_ffp, metadata.defaults_version
+    )
     emod3d_parameters = EMOD3DParameters.read_from_realisation_or_defaults(
         realisation_ffp, metadata.defaults_version
     )
     e3d_par_values = (
         emod3d_parameters.to_dict()
-        | emod3d_domain_parameters(domain_parameters)
+        | emod3d_domain_parameters(resolution, domain_parameters)
         | emod3d_duration_parameters(
+            resolution,
             domain_parameters,
             min_vs=velocity_model_parameters.min_vs,
             dtts=emod3d_parameters.dtts,

workflow/scripts/hf_sim.py
@@ -52,6 +52,7 @@ from workflow.realisations import (
     HFConfig,
     HFVelocityModel1D,
     RealisationMetadata,
+    Resolution,
     Seeds,
 )
 
@@ -61,6 +62,7 @@ app = typer.Typer()
 def build_hf_input(
     stoch_ffp: Path,
     velocity_model: Path,
+    resolution: Resolution,
     hf_config: HFConfig,
     seeds: Seeds,
     domain_parameters: DomainParameters,
@@ -98,7 +100,7 @@ def build_hf_input(
         f"{hf_config.nbu} {hf_config.ift} {hf_config.flo} {hf_config.fhi}",
         "{seed}",
         1,  # one station in the input
-        f"{domain_parameters.duration} {hf_config.dt} {hf_config.fmax} {hf_config.kappa} {hf_config.qfexp}",
+        f"{domain_parameters.duration} {resolution.dt} {hf_config.fmax} {hf_config.kappa} {hf_config.qfexp}",
         f"{hf_config.rvfac} {hf_config.rvfac_shal} {hf_config.rvfac_deep} {hf_config.czero} {hf_config.calpha}",
         f"{hf_config.mom or -1} {hf_config.rupv or -1}",
         stoch_ffp,
@@ -289,6 +291,9 @@ def run_hf(
     hf_config = HFConfig.read_from_realisation_or_defaults(
         realisation_ffp, metadata.defaults_version
     )
+    resolution = Resolution.read_from_realisation_or_defaults(
+        realisation_ffp, metadata.defaults_version
+    )
 
     stations = pd.read_csv(
         station_file,
@@ -306,11 +311,13 @@ def run_hf(
     stations["seed"] = np.int32(seeds.hf_seed) ^ station_hashes
     velocity_model_path = work_directory / "velocity_model"
     velocity_model.write_velocity_model(velocity_model_path)
-    nt = int(np.float32(domain_parameters.duration) / np.float32(hf_config.dt))  # Match Fortran's single-precision for consistent nt calculation
+    nt = int(
+        np.float32(domain_parameters.duration) / np.float32(resolution.dt)
+    )  # Match Fortran's single-precision for consistent nt calculation
     waveform = np.empty((3, len(stations), nt), dtype=np.float32)
 
     hf_input_template = build_hf_input(
-        stoch_ffp, velocity_model_path, hf_config, seeds, domain_parameters
+        stoch_ffp, velocity_model_path, resolution, hf_config, seeds, domain_parameters
     )
 
     stations["epicentre_distance"] = np.nan
@@ -359,7 +366,7 @@ def run_hf(
         attrs={
             "start_sec": start_sec,
             "nt": nt,
-            "dt": hf_config.dt,
+            "dt": resolution.dt,
             "units": "cm/s^2",
         },
     ).to_netcdf(out_file, engine="h5netcdf")