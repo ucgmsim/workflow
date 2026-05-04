918fb6c506a0c4c6a2daaf0a998007b39079d75e
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2026-05-05 10:12:02 +1200

    fix(realisation-to-srf): Calculate the top-depth of each layer rather than the bottom depth

--- workflow/scripts/realisation_to_srf.py
+++ workflow/scripts/realisation_to_srf.py
@@ -653,7 +653,9 @@ def generate_point_source_srf(
     moment_newton_metre = moment.magnitude_to_moment(magnitude)
 
     velocity_model_df = params.velocity_model_1d.model
-    velocity_model_df["depth_km"] = velocity_model_df["thickness"].cumsum()
+    velocity_model_df["depth_km"] = (
+        velocity_model_df["thickness"].cumsum() - velocity_model_df["thickness"]
+    )
 
     # Get the source depth
     # divide by 1000 to convert depth from meters to kilometers
